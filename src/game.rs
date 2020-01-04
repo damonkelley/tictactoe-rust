25e7f90e0eca714648778b64a84f9c3680f4f1e1
Damon Kelley <damon.kelley@gmail.com>
2020-01-04 09:30:16 -0500

    Allow TicTacToe to determine the next move

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,8 @@
 use crate::board::{Board, Space};
 use crate::token::Token;
+use std::cell::RefCell;
+
+use std::fmt;
 
 pub trait Game: std::fmt::Debug {
     fn outcome(&self) -> Option<Outcome>;
@@ -12,9 +15,36 @@ pub enum Outcome<'a> {
     Draw,
 }
 
-#[derive(Debug)]
+trait GetInput {
+    fn get(&self) -> Option<Space>;
+}
+
+struct Turn<'a> {
+    tokens: RefCell<Box<dyn Iterator<Item = Token<'a>> + 'a>>,
+}
+
+impl<'a> Turn<'a> {
+    fn new(tokens: &'a Vec<Token<'a>>) -> Self {
+        Turn {
+            tokens: RefCell::new(Box::new(tokens.iter().copied().cycle())),
+        }
+    }
+
+    fn next(&self) -> Option<Token<'a>> {
+        self.tokens.borrow_mut().next()
+    }
+}
+
 struct TicTacToe<'a> {
-    board: Board<'a>,
+    board: &'a Board<'a>,
+    get_input: &'a dyn GetInput,
+    turn: Turn<'a>,
+}
+
+impl<'a> fmt::Debug for TicTacToe<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Game {{ board: {:?} }}", self.board)
+    }
 }
 
 impl<'a> Game for TicTacToe<'a> {
@@ -27,6 +57,11 @@ impl<'a> Game for TicTacToe<'a> {
     }
 
     fn make_move(&self) -> &dyn Game {
+        let maybe_input = self.get_input.get();
+        let maybe_token = self.turn.next();
+
+        maybe_input.and_then(|space| maybe_token.map(|token| self.board.put(space, token)));
+
         return self;
     }
 }
@@ -35,8 +70,16 @@ type Combination = Vec<Space>;
 type Combinations = Vec<Combination>;
 
 impl<'a> TicTacToe<'a> {
-    pub fn new(board: Board) -> TicTacToe {
-        TicTacToe { board }
+    pub fn new(
+        board: &'a Board<'a>,
+        get_input: &'a dyn GetInput,
+        tokens: &'a Vec<Token<'a>>,
+    ) -> TicTacToe<'a> {
+        TicTacToe {
+            board,
+            get_input,
+            turn: Turn::new(tokens),
+        }
     }
 
     fn combinations(&self) -> Combinations {
@@ -79,9 +122,33 @@ impl<'a> TicTacToe<'a> {
 mod tests {
     use super::*;
 
+    struct StubbedInput<'a> {
+        moves: RefCell<Box<dyn Iterator<Item = Space> + 'a>>,
+    }
+
+    impl<'a> GetInput for StubbedInput<'a> {
+        fn get(&self) -> Option<Space> {
+            self.moves.borrow_mut().next()
+        }
+    }
+
+    fn stubbed_input<'a>(moves: &'a Vec<Space>) -> StubbedInput<'a> {
+        StubbedInput {
+            moves: RefCell::new(Box::new(moves.iter().copied())),
+        }
+    }
+
     #[test]
     fn outcome_will_be_none_when_the_game_is_in_progress() {
-        assert_eq!(TicTacToe::new(Board::new()).outcome(), None)
+        assert_eq!(
+            TicTacToe::new(
+                &Board::new(),
+                &stubbed_input(&vec![1]),
+                &vec![Token::new("X"), Token::new("O")]
+            )
+            .outcome(),
+            None
+        )
     }
 
     #[test]
@@ -98,6 +165,9 @@ mod tests {
         ];
 
         for combination in combinations.iter() {
+            let moves = vec![1];
+            let get_input = stubbed_input(&moves);
+            let tokens = vec![Token::new("X"), Token::new("O")];
             let board = Board::new();
 
             combination.iter().for_each(|&space| {
@@ -105,14 +175,17 @@ mod tests {
             });
 
             assert_eq!(
-                TicTacToe::new(board).outcome(),
+                TicTacToe::new(&board, &get_input, &tokens).outcome(),
                 Some(Outcome::Winner(Token::new("X")))
-            )
+            );
         }
     }
 
     #[test]
     fn outcome_will_be_a_tie_when_there_is_no_winner_and_no_more_spaces() {
+        let moves = vec![1];
+        let get_input = stubbed_input(&moves);
+        let tokens = vec![Token::new("X"), Token::new("O")];
         let board = Board::new();
 
         board
@@ -126,6 +199,48 @@ mod tests {
             .put(8, Token::new("X"))
             .put(9, Token::new("O"));
 
-        assert_eq!(TicTacToe::new(board).outcome(), Some(Outcome::Draw))
+        assert_eq!(
+            TicTacToe::new(&board, &get_input, &tokens).outcome(),
+            Some(Outcome::Draw)
+        );
+    }
+
+    #[test]
+    fn make_move_will_place_a_token_on_the_board() {
+        let moves = vec![1, 2, 3, 4, 5];
+        let get_input = stubbed_input(&moves);
+        let tokens = vec![Token::new("X"), Token::new("O")];
+
+        let board = Board::new();
+
+        let game = TicTacToe::new(&board, &get_input, &tokens);
+
+        (1..=5).map(|_| game.make_move()).for_each(drop);
+
+        assert_eq!(
+            board.get(1),
+            Some(Token::new("X")),
+            "It will make the first move"
+        );
+        assert_eq!(
+            board.get(2),
+            Some(Token::new("O")),
+            "It will make the second move"
+        );
+        assert_eq!(
+            board.get(3),
+            Some(Token::new("X")),
+            "It will make the third move"
+        );
+        assert_eq!(
+            board.get(4),
+            Some(Token::new("O")),
+            "It will make the fourth move"
+        );
+        assert_eq!(
+            board.get(5),
+            Some(Token::new("X")),
+            "It will make the fifth move"
+        );
     }
 }
